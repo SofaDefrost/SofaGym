6d6cced4d95ef07c87bcf92114ecdde1bb60ecf4
Samuel Youssef <samuelm.youssef@gmail.com>
2023-04-20 14:24:25 +0200

    Modify test script

--- test_env.py
+++ test_env.py
@@ -16,6 +16,8 @@ import sys
 import os
 import time
 import gym
+import argparse
+from colorama import Fore
 
 import sofagym
 from sofagym.envs import *
@@ -35,77 +37,83 @@ name = {
         4:'catchtheobject-v0',
         5:'concentrictuberobot-v0',
         6:'diamondrobot-v0',
-        7:'gripper-v0',       
+        7:'gripper-v0',
         8:'maze-v0',
         9:'multigaitrobot-v0',
         10:'simple_maze-v0',
         11:'stempendulum-v0',
-        12:'trunk-v0',         
-        13:'trunkcup-v0',    
-        } 
+        12:'trunk-v0',
+        13:'trunkcup-v0',
+        }
 
-num = 5
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--env", help="Name of the environment",
+                        type=str, required=True)
+    parser.add_argument("-ep", "--episodes", help="Number of episodes",
+                        type=int, required=False, default=100)
+    parser.add_argument("-s", "--steps", help="Number of steps per episodes",
+                        type=int, required=False, default=100)    
+    args = parser.parse_args()
 
-env_name = name[num]
-print("Start env ", env_name)
+    env_name = args.env
+    episodes = args.episodes
+    steps = args.steps
 
-env = gym.make(env_name)
-env.configure({"render":1})
-env.configure({"dt":0.01})
-env.reset()
+    try:
+        env = gym.make(env_name)
+    except Exception:
+        print(Fore.RED + '[ERROR]   ' + Fore.RESET + "Environment name does not exist.")
+        raise SystemExit(f"Available environments:\n{[value for value in name.values()]}")
 
-env.render()
-done = False
+    print("Start env ", env_name)
 
-print("Start ...")
-for i in range(10000000):
-    print("\n--------------------------------")
-    print("EPISODE - ", i)
-    print("--------------------------------\n")
-    idx = 0
-    tot_reward = 0
-    tot_rtf = 0
-    done = False
-    while not done and idx < 100:
-        idx += 1
-        #multigaitrobot: [rd.uniform(-1, 1) for i in range(5)] - strat_multi[idx-1]
-        #gripper: rd.randint(0,7)
-        #trunk: rd.randint(0,15)
-        #trunkcup: rd.randint(0,15)
-        #diamondrobot: rd.randint(0,7)
-        #maze: rd.randint(0,6)
-        #simple_maze: rd.randint(0,3)
-        #concentrictuberobot: rd.randint(0,11)
-        #CartStem [rd.uniform(-1, 1)]
-        #action = strat_multi[idx-1]
-
-        action = env.action_space.sample()
-        
-        start_time = time.time()
-        state, reward, done, info = env.step(action)
-        step_time = time.time()-start_time
-        print("[INFO]   >>> Time:", step_time)
-        
-        rtf = env.config["dt"]*env.config["scale_factor"]/step_time
-        print("[INFO]   >>> RTF:", rtf)
-        
-        tot_reward+= reward
-        tot_rtf+= rtf
-        
-        env.render()
-
-        print("Step ", idx, " action : ", action, " reward : ", reward, " done:", done)
-
-    print("[INFO]   >>> TOTAL REWARD IS:", tot_reward)
-    print("[INFO]   >>> FINAL REWARD IS:", reward)
-    print("[INFO]   >>> MEAN RTF IS:", tot_rtf/idx)
-    memoryUse = py.memory_info()[0]/2.**30
-    print("[INFO]   >>> Memory usage:", memoryUse)
-    print("[INFO]   >>> Object size:", sys.getsizeof(env))
-
+    env.configure({"render":1})
+    env.configure({"dt":0.01})
     env.reset()
 
+    env.render()
+    done = False
 
-print(">> TOTAL REWARD IS:", tot_reward)
-env.close()
-print("... End.")
+    print("Start ...")
+    for i in range(episodes):
+        print("\n--------------------------------")
+        print("EPISODE - ", i+1)
+        print("--------------------------------\n")
+        idx = 0
+        tot_reward = 0
+        tot_rtf = 0
+        done = False
+        while not done and idx < steps:
+            idx += 1
+
+            action = env.action_space.sample()
+
+            start_time = time.time()
+            state, reward, done, info = env.step(action)
+            step_time = time.time()-start_time
+            print("[INFO]   >>> Time:", step_time)
+
+            rtf = env.config["dt"]*env.config["scale_factor"]/step_time
+            print("[INFO]   >>> RTF:", rtf)
+
+            tot_reward+= reward
+            tot_rtf+= rtf
+
+            env.render()
+
+            print("Step ", idx, " action : ", action, " reward : ", reward, " done:", done)
+
+        print("[INFO]   >>> TOTAL REWARD IS:", tot_reward)
+        print("[INFO]   >>> FINAL REWARD IS:", reward)
+        print("[INFO]   >>> MEAN RTF IS:", tot_rtf/idx)
+        memoryUse = py.memory_info()[0]/2.**30
+        print("[INFO]   >>> Memory usage:", memoryUse)
+        print("[INFO]   >>> Object size:", sys.getsizeof(env))
+
+        env.reset()
+
+
+    print(">> TOTAL REWARD IS:", tot_reward)
+    env.close()
+    print("... End.")
