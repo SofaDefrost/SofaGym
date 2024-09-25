afd0ec5172c556892cfb79c521ab6575464f442f
samuelmyoussef <samuelm.youssef@gmail.com>
2024-09-26 00:35:42 +0200

    add some default configs

--- sofagym/envs/Gripper/GripperEnv.py
+++ sofagym/envs/Gripper/GripperEnv.py
@@ -26,6 +26,7 @@ class GripperEnv:
     # Setting a default configuration
     path = os.path.dirname(os.path.abspath(__file__))
     metadata = {'render.modes': ['human', 'rgb_array']}
+    dim_state = 31
     DEFAULT_CONFIG = {"scene": "Gripper",
                       "deterministic": True,
                       "source": [0, -80, 350],
@@ -47,6 +48,8 @@ class GripperEnv:
                       "start_from_history": None,
                       "python_version": sys.version,
                       "dt": 0.01,
+                      "nb_actions": 8,
+                      "dim_state": dim_state,
                       "randomize_states": False,
                       "use_server": False
                       }
@@ -55,11 +58,11 @@ class GripperEnv:
         self.use_server = self.DEFAULT_CONFIG["use_server"]
         self.env = ServerEnv(self.DEFAULT_CONFIG, config, root=root) if self.use_server else AbstractEnv(self.DEFAULT_CONFIG, config, root=root)
 
-        nb_actions = 8
+        nb_actions = self.env.config["nb_actions"]
         self.env.action_space = spaces.Discrete(nb_actions)
         self.nb_actions = str(nb_actions)
 
-        dim_state = 31
+        dim_state = self.env.config["dim_state"]
         low_coordinates = np.array([-1]*dim_state)
         high_coordinates = np.array([1]*dim_state)
         self.env.observation_space = spaces.Box(low_coordinates, high_coordinates, dtype=np.float32)
