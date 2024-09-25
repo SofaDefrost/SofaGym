1f923ae7575932e06077267ac8bb4b733e78b41a
samuelmyoussef <samuelm.youssef@gmail.com>
2024-09-25 17:16:19 +0200

    goal init and update

--- sofagym/envs/CatchTheObject/CatchTheObjectEnv.py
+++ sofagym/envs/CatchTheObject/CatchTheObjectEnv.py
@@ -66,6 +66,9 @@ class CatchTheObject:
 
         self.initialize_states()
 
+        if self.env.config["goal"]:
+            self.init_goal()
+
         nb_actions = self.env.config["nb_actions"]
         low = np.array([-1]*1)
         high = np.array([1]*1)
@@ -106,14 +109,23 @@ class CatchTheObject:
         """
         return self.env.config["init_states"]
 
+    def init_goal(self):
+        # Set a new random goal from the list
+        goalList = self.env.config["goalList"]
+        id_goal = self.env.np_random.choice(range(len(goalList)))
+        self.env.config.update({'goal_node': id_goal})
+        self.env.goal = goalList[id_goal]
+        self.env.config.update({'goalPos': self.env.goal})
+
     def reset(self):
         """Reset simulation.
         """
         self.initialize_states()
-        
-        self.env.reset()
 
-        self.env.config.update({'goalPos': self.env.goal})
+        if self.env.config["goal"]:
+            self.init_goal()
+
+        self.env.reset()
         
         if self.use_server:
             obs = start_scene(self.env.config, self.nb_actions)
