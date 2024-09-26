5795fb73c21f9ae07cb9fcf2c260d73db83af0d0
samuelmyoussef <samuelm.youssef@gmail.com>
2024-09-26 05:26:39 +0200

    remove non-overriden methods

--- sofagym/envs/Gripper/GripperEnv.py
+++ sofagym/envs/Gripper/GripperEnv.py
@@ -55,7 +55,9 @@ class GripperEnv:
                       "use_server": False
                       }
 
-    def __init__(self, config = None, root=None, use_server: Optional[bool]=False):
+    def __init__(self, config = None, root=None, use_server: Optional[bool]=None):
+        if use_server is not None:
+            self.DEFAULT_CONFIG.update({'use_server': use_server})
         self.use_server = self.DEFAULT_CONFIG["use_server"]
         self.env = ServerEnv(self.DEFAULT_CONFIG, config, root=root) if self.use_server else AbstractEnv(self.DEFAULT_CONFIG, config, root=root)
         
@@ -64,13 +66,11 @@ class GripperEnv:
         if self.env.config["goal"]:
             self.init_goal()
 
-        nb_actions = self.env.config["nb_actions"]
-        self.env.action_space = spaces.Discrete(nb_actions)
-        self.nb_actions = str(nb_actions)
+        self.env.action_space = spaces.Discrete(self.env.nb_actions)
+        self.nb_actions = str(self.env.nb_actions)
 
-        dim_state = self.env.config["dim_state"]
-        low_coordinates = np.array([-1]*dim_state)
-        high_coordinates = np.array([1]*dim_state)
+        low_coordinates = np.array([-1]*self.env.dim_state)
+        high_coordinates = np.array([1]*self.env.dim_state)
         self.env.observation_space = spaces.Box(low_coordinates, high_coordinates, dtype=np.float32)
 
         if self.env.root is None and not self.use_server:
@@ -81,35 +81,6 @@ class GripperEnv:
         # assume it is implemented by self.instance
         return self.env.__getattribute__(name)
     
-    def initialize_states(self):
-        if self.env.config["randomize_states"]:
-            self.init_states = self.randomize_init_states()
-            self.env.config.update({'init_states': list(self.init_states)})
-        else:
-            self.init_states = self.env.config["init_states"]
-
-    def randomize_init_states(self):
-        """Randomize initial states.
-
-        Returns:
-        -------
-            init_states: list
-                List of random initial states for the environment.
-        
-        Note:
-        ----
-            This method should be implemented according to needed random initialization.
-        """
-        return self.env.config["init_states"]
-
-    def init_goal(self):
-        # Set a new random goal from the list
-        goalList = self.env.config["goalList"]
-        id_goal = self.env.np_random.choice(range(len(goalList)))
-        self.env.config.update({'goal_node': id_goal})
-        self.env.goal = goalList[id_goal]
-        self.env.config.update({'goalPos': self.env.goal})
-
     def reset(self):
         """Reset simulation.
         """
@@ -127,16 +98,3 @@ class GripperEnv:
             state = np.array(self.env._getState(self.env.root), dtype=np.float32)
         
         return state
-
-    def get_available_actions(self):
-        """Gives the actions available in the environment.
-
-        Parameters:
-        ----------
-            None.
-
-        Returns:
-        -------
-            list of the action available in the environment.
-        """
-        return self.env.action_space
